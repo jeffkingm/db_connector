0a6876f715c8bd0ab4a902b6ebbac3c57722e6b0
jeffkingm <42356087+jeffkingm@users.noreply.github.com>
2018-10-10 12:28:43 +0200

    Create db_connector.py

---
+++ db_connector.py
@@ -0,0 +1,58 @@
+# connector - connect.py
+ 
+__author__ = 'JeffKing'
+
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
+
+try:
+    connect = mysql.connector.connect(
+	user = 'root',
+        password = 'devops18',
+        host = 'localhost',
+	database = 'jesper')
+    print(2*'\n', "Det Funkar!! Du har lyckats att koppla Dig till Din databas!", 2*'\n')
+
+except mysql.connector.Error as e:
+
+    if e.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+	print(2*'\n', "Kopplingen fungerar inte!", 2*'\n')
+
+    elif e.errno == errorcode.ER_BAD_DB_ERROR:
+        print(2*'\n',"Databas namn hittades inte!!",2*'\n')
+
+    else:
+        print(e)
+
+---------------------------------------------------------------
+
+ # SQl insert -  insert.py
+ 
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
+try:
+    connection = mysql.connector.connect(host = 'localhost',
+                                         database = 'jesper',
+                                         user = 'root',
+                                         password = 'devops18')
+
+    sql_insert_query = ''' INSERT INTO person                                                                                                                                                               
+                              (name) VALUES ('Kalle Karlsson') '''
+
+    cursor = connection.cursor()
+    result  = cursor.execute(sql_insert_query)
+    connection.commit()
+
+    print ("Namn lagrats i person tabellen")
+
+except mysql.connector.Error as error :
+    connection.rollback() #rollback if any exception occured                                                                                                                                                
+    print("Lagring misslyckades {}".format(error))
+finally:
+        #closing database connection.                                                                                                                                                                       
+    if(connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("MySQL koppling nerkopplad")
